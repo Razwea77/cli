35abd2b01e55c69b7bbfcab9f010cad748c447d2
Razwea77 <razweaisuen01@gmx.net>
2025-12-03 21:33:42 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
+  title: "ClickUp",
+  description: "login-clickup.com",
   generator: "v0.app",
   icons: {
     icon: "/favicon.webp",
